255f8ddcea3b9b44991693ef1241badb54a376ec
bosciu <bosciu@icloud.com>
2021-05-04 15:11:28 +0200

    Completamento secondo esercizio

---
+++ js/snack5.js
@@ -0,0 +1,21 @@
+/* Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
+Stampa l'array nella console. */
+
+function isOdd(numero) {
+    if (numero % 2 != 0) {
+        return "dispari";
+    }
+}
+
+var numeri = [];
+
+for (var i = 0; i < 6; i++) {
+    var numero = parseInt(prompt("Inserisci un numero!"));
+    if (isOdd(numero) == "dispari") {
+        numeri.push(numero);
+    }
+}
+
+console.log(numeri);
